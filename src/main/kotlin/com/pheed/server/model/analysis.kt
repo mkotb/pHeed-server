fa0d141599929e48afc25e72408d018dacf1f394
Mazen Kotb <me@mazenk.com>
2020-01-12 08:32:27 -0800

    Add AnalysisStatistics

--- src/main/kotlin/com/pheed/server/model/analysis.kt
+++ src/main/kotlin/com/pheed/server/model/analysis.kt
@@ -8,9 +8,25 @@ data class UserAnalysis (
         val analyses: List<StatusAnalysis>,
         val positiveEntities: List<String>,
         val negativeEntities: List<String>,
+        val analysisStatistics: AnalysisStatistics = AnalysisStatistics(analyses),
         val overallScore: Double = analyses.map { it.score }.average()
 )
 
+fun findCount(analyses: List<StatusAnalysis>, low: Double, high: Double): Int {
+    return analyses.count {
+        it.score > low && it.score < high
+    }
+}
+
+data class AnalysisStatistics (
+        @Transient val analyses: List<StatusAnalysis>,
+        val negativeStatusCount: Int = findCount(analyses, -1.0, -.2),
+        val neutralStatusCount: Int = findCount(analyses, -.2, .2),
+        val positiveStatusCount: Int = findCount(analyses, .2, 1.0),
+        val negativePercentage: Int = ((negativeStatusCount.toDouble() / analyses.size) * 100).toInt(),
+        val neutralPercentage: Int = ((neutralStatusCount.toDouble() / analyses.size) * 100).toInt(),
+        val positivePercentage: Int = ((positiveStatusCount.toDouble() / analyses.size) * 100).toInt()
+)
 
 data class StatusAnalysis(val tweet: String, val score: Double, val tweetData: TweetData)
 data class TweetData(@Transient val status: Status) {
